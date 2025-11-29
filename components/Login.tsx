20025c4445f14f2849c75c7d315012fa04841cc5
Christian Gonzalez <chrisgo@bu.edu>
2025-11-29 00:29:14 -0500

    comment

--- components/Login.tsx
+++ components/Login.tsx
@@ -4,6 +4,10 @@ export default function LoginButton() {
   return (
     <>
       <Button>
+        {/*
+          i originally used a <Link> tag here but the next.js prefetching was throwing errors when
+          clicking the button so i switched to an <a> tag for simplicity. hope that's fine
+        */}
         <a href={`/api/auth/github`}>Sign in with GitHub</a>
       </Button>
     </>
